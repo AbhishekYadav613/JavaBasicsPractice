1ba981b8e92f714c7cbf0cffa4b85b5b369e834f
AbhishekYadav613 <abhishekyadav613@gmail.com>
2024-03-03 16:47:16 +0530

    CalculationCheck

---
+++ src/test/java/day3/Weekday1.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class Weekday1 {
+
+    public static void main(String[] args) {
+
+    int a, b, c, d, sum, multiple, sub, div;
+    a= 10; b=20; c=12; d=30; sum=a+b; multiple=c*a; div=d/a; sub= b-a;
+
+    System.out.println("Sum of two numbers is: " +sum);
+    System.out.println("Multiplication of two numbers is: " +multiple);
+        System.out.println("Difference of two numbers is: " +sub);
+        System.out.println("Division of two numbers is: " +div);
+
+
+    }
+}
